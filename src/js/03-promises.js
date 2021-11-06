7e5c2d18f14b52da077fc8f071a5c3c4086eeb8e
MaksStelmakh <step4ick2017@gmail.com>
2021-11-06 17:45:23 +0200

    Начал 3 задание

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,5 @@
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -5,4 +7,23 @@ function createPromise(position, delay) {
   } else {
     // Reject
   }
+}
+
+const form = document.querySelector(`.form`)
+const submitBtn = document.querySelector(`button`)
+
+form.addEventListener(`input`, checkForm)
+submitBtn.addEventListener(`click`, submitFunc)
+
+
+function checkForm() {
+  const valueDelay = form.delay.value
+  const valueStep = form.step.value
+  const valueAmount = form.amount.value
+  return {valueDelay, valueStep, valueAmount}
+}
+
+function submitFunc(ev) {
+  ev.preventDefault()
+  console.log(checkForm())
 }
