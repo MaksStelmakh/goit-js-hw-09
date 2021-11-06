55fed75ea6095cfa5684b662b4b8c381e04f74c4
MaksStelmakh <step4ick2017@gmail.com>
2021-11-06 12:07:23 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startButton = document.querySelector(`button[data-start]`)
+const stopButton = document.querySelector(`button[data-stop]`)
+const background = document.querySelector(`body`)
+
+let timerId = null
+
+stopButton.setAttribute(`disabled`, true)
+
+startButton.addEventListener(`click`, onChangeBacgroundColor)
+stopButton.addEventListener(`click`, stopChangeBackgroundColor)
+
+function onChangeBacgroundColor() {
+    stopButton.removeAttribute(`disabled`)
+    timerId = setInterval(() => {
+       background.style.backgroundColor = getRandomHexColor() 
+    }, 1000,)
+    startButton.setAttribute(`disabled`, true)
+}
+
+function stopChangeBackgroundColor() {
+    clearInterval(timerId);
+    stopButton.setAttribute(`disabled`, true)
+    startButton.removeAttribute(`disabled`)
+}
