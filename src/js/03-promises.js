bc0e4edf5daf7df2bc8de79d8f3851c6a9277c5f
MaksStelmakh <step4ick2017@gmail.com>
2021-11-07 19:53:17 +0200

    3 task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,29 +1,58 @@
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const form = document.querySelector(`.form`)
+
+form.addEventListener(`submit`, submitFunc)
+
+
+let delay = 0
+let position = 0
+
+function submitFunc(event) {
+  event.preventDefault();
+  const formElement = event.currentTarget.elements;
+  const firstDelay = Number(formElement.delay.value);
+  const step = Number(formElement.step.value);
+  const amount = Number(formElement.amount.value);
+
+  makePromises(firstDelay, step, amount)
+}
+
+function makePromises(firstDelay, step, amount) {
+  for (let i = 1; i <= amount; i += 1) {
+    if (i === 1) {
+      delay = firstDelay
+    } else {
+      delay += step
+    }
+    position = i
+
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`)
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`)
+      })
   }
 }
 
-const form = document.querySelector(`.form`)
-const submitBtn = document.querySelector(`button`)
-
-form.addEventListener(`input`, checkForm)
-submitBtn.addEventListener(`click`, submitFunc)
-
-
-function checkForm() {
-  const valueDelay = form.delay.value
-  const valueStep = form.step.value
-  const valueAmount = form.amount.value
-  return {valueDelay, valueStep, valueAmount}
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({position, delay})
+      } else {
+        reject({position, delay})
+      }
+    }, delay)
+    })
 }
 
-function submitFunc(ev) {
-  ev.preventDefault()
-  console.log(checkForm())
-}
+
+
+
+
